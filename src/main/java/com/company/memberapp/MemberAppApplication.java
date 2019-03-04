c4759305bd87152d1cf00d534fa1b7c8691d93c5
Digya Mandala Putra <digyamp@gmail.com>
2019-03-04 17:20:24 +0700

    first commit

---
+++ src/main/java/com/company/memberapp/MemberAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.memberapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberAppApplication.class, args);
+	}
+
+}
